91c39f1a749d887d88bd169507937cea8b376f57
Marvin Danig <marvin@marvindanig.com>
2016-05-19 10:17:49 -0400

    Fixing length() method as per shellJS ls('-d', path) api

--- crust/plugins/length.js
+++ crust/plugins/length.js
@@ -2,7 +2,7 @@ require('shelljs/global');
 
 function length() {
   var path = require('path');
-  return ls(path.join('manuscript','page-*')).length;
+  return ls('-d', path.join('manuscript','page-*')).length;
 }
 
 module.exports.length = length;
